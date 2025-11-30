3d7ab42eaf8d6984274fc0ea6b09f5bba120d6ee
HarisaranS <harisaran777s@gmail.com>
2025-11-30 16:31:51 +0530

    Enhance run.sh for web enumeration
    Add directory creation and subdomain harvesting logic

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Web Enumeration using Bash Scripting"
+url=$1
+
+if [ ! -d "$url" ];then
+			mkdir $url
+fi
+
+if [ ! -d "$url/recon" ];then 
+			mkdir $url/recon
+fi
+
+echo "[+] Harvesting subdomains with assetfinder..."
+assetfinder $url >> $url/recon/asset.txt
+cat $url/recon/asset.txt | grep $url | sort -u >> $url/recon/final.txt
+rm $url/recon/asset.txt
