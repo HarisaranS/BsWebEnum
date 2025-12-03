33b151ed01427c8b03d427d169a20ff002fdb308
HarisaranS <harisaran777s@gmail.com>
2025-12-03 06:16:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,10 @@ if [ ! -d "$url/recon" ];then
 		mkdir $url/recon
 fi
 
+if [ ! -d "$url/recon/wayback" |;then
+		mkdir $url/recon/wayback
+fi
+
 echo "[+] Harvesting subdomains with assetfinder..."
 assetfinder $url >> $url/recon/asset.txt
 cat $url/recon/asset.txt | grep $url | sort -u >> $url/recon/final.txt
@@ -23,5 +27,11 @@ rm $url/recon/f.txt
 echo "[+] Probing for alive domains..."
 cat $url/recon/final.txt | httprobe -s | sed 's/:443$//' >> $url/recon/alive.txt
 
+echo "[+] Pulling and compile all possible params found in wayback data..."
+cat $url/recon/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f1 | sort -u >> $url/recon/wayback/wayback_params.txt
+for line in $(cat $uel/recon/wayback/wayback_params.txt):do echo $line'=';done
+
 echo "[+] Done!"
 echo "Alive domains saved in : $url/recon/alive.txt"
+
+
