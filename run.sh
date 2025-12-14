d542a2bf07c795ff7d62f1f913fb60a9e0b68ad5
HarisaranS <harisaran777s@gmail.com>
2025-12-14 13:07:05 +0530

    Enhance run.sh with better error handling
    Refactor run.sh to improve error handling and streamline directory creation.

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-url=$1
+url="$1"
 
-if [ ! -d "$url" ]; then
-		mkdir $url
+set -eou pipefail
+
+if [ -z "$url" ]; then 
+	echo "Usage : $0 <domain>"
+	exit 1
 fi
 
-if [ ! -d "$url/recon" ]; then 
-		mkdir $url/recon
-fi
-
-if [ ! -d "$url/recon/wayback" ]; then
-		mkdir $url/recon/wayback
-fi
+echo "[+] Setting up directories..."
+mkdir -p "$url/recon/wayback"
 
 echo "[+] Harvesting subdomains with assetfinder..."
-assetfinder $url >> $url/recon/asset.txt
-cat $url/recon/asset.txt | grep $url | sort -u >> $url/recon/final.txt
-rm $url/recon/asset.txt
+assetfinder --subs-only "$url" | sort -u > "$url/recon/asset.txt"
 
 echo "[+] Harvesting subdomains with Amass..."
-amass enum -d $url >> $url/recon/f.txt
-sort -u $url/recon/f.txt >> $url/recon/final.txt
-rm $url/recon/f.txt
+amass enum -d "$url" -silent | sort -u > "$url/recon/amass.txt"
+
+echo "[+] Combining subdomains..."
+cat "$url/recon/"{asset,amass}.txt | sort -u > "$url/recon/final.txt"
+rm "$url/recon/"{asset,amass}.txt
 
 echo "[+] Probing for alive domains..."
-cat $url/recon/final.txt | httprobe -s | sed 's/:443$//' >> $url/recon/alive.txt
+cat "$url/recon/final.txt" | httprobe -s -p https:443 -p http:80 | sed 's|https\?://||' | sort -u > "$url/recon/alive.txt"
 
-cat "$url/recon/wayback/wayback_output.txt" | grep '?*=' | cut -d '=' -f1 | sort -u > "$url/recon/wayback/wayback_params.txt"
-echo "[+] Pulling and compiling all possible params..."
-for line in $(cat "$url/recon/wayback/wayback_params.txt"); do echo "$line="; done
+echo "[+] Pulling Wayback URLs..."
+cat "$url/recon/alive.txt" | waybackurls > "$url/recon/wayback/wayback_output.txt"
+
+echo "[+] Extracting parameters..."
+grep '\?' "$url/recon/wayback/wayback_output.txt" | cut -d '?' -f2 | tr '&' '\n' | cut -d "=" -f1 | sort -u > "$url/recon/wayback/wayback_params.txt"
 
 echo "[+] Done!"
 echo "Alive domains saved in : $url/recon/alive.txt"
